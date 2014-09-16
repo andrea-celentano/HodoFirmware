dd5defff0795c98d706357a0af7c41e9dc42f67c
Andrea Celentano <andrea.celentano@ge.infn.it>
2014-09-16 11:52:46 +0200

    First committ

---
+++ HodoFirmware.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# FT configuration
+CND_ARTIFACT_DIR_FT=dist/FT/production
+CND_ARTIFACT_NAME_FT=HodoFirmware.X.production.hex
+CND_ARTIFACT_PATH_FT=dist/FT/production/HodoFirmware.X.production.hex
+CND_PACKAGE_DIR_FT=${CND_DISTDIR}/FT/package
+CND_PACKAGE_NAME_FT=hodofirmware.x.tar
+CND_PACKAGE_PATH_FT=${CND_DISTDIR}/FT/package/hodofirmware.x.tar
